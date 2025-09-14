b0161577954ef23914d9c261aad4078e440bef48
yashbarbate08 <yashbarbate8@gmail.com>
2025-09-14 16:01:25 +0530

    backend render url and setting up cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://uber-clone-frontend-eta.vercel.app/",
+      "https://uber-clone-frontend-eta.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
