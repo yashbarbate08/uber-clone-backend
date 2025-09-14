a1fd8f6890c570e1ec72756a2f38d2a2bae3dcac
yashbarbate08 <yashbarbate8@gmail.com>
2025-09-14 16:07:04 +0530

    cors setup

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(
       "https://uber-clone-frontend-eta.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
