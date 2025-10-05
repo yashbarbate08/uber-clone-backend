5f5912af285d488c9a0c8030be1169dbf3d2dbb7
Yash Barbate <yashbarbate8@gmail.com>
2025-10-05 14:56:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      " https://uber-clone-frontend-mnq7hbgth-yash-barbates-projects.vercel.app", // frontend URL
+      "https://uber-clone-frontend-mnq7hbgth-yash-barbates-projects.vercel.app", // frontend URL
      
       "http://localhost:5173", // local dev (optional)
     ],
